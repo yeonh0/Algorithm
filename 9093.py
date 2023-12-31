28b044169cd3c33f9bafa43c801eba91fff5bb5c
yeonh0 <nanyeonho@gmail.com>
2023-12-31 22:15:09 +0900

    스트링에 [::-1] 하면 뒤집힘. 리스트에서 순서 반대로 할 때 사용함

--- 9093.py
+++ 9093.py
@@ -11,11 +11,5 @@ n = int(input())
 for _ in range(n):
     msg = input().split(' ')
     for i in msg:
-        i = list(i)
-        while(1):
-            if (len(i) == 0):
-                break
-            else:
-                print(i.pop(), end='')
-        print(' ', end='')
+        print(i[::-1], end=" ")
     print()
