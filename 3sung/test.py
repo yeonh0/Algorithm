40894b6d31dddfacd86f16669565285c43b55e7c
yeonh0 <nanyeonho@gmail.com>
2024-03-21 13:19:12 +0900

    DFS 예제 코드

--- 3sung/test.py
+++ 3sung/test.py
@@ -1,18 +1,25 @@
-import sys 
-input = sys.stdin.readline
+def dfs(graph, v, visited):
+    # 방문한 노드 표시
+    visited[v] = True
+    print(v, end=" ")
 
+    # 인접 노드 중 방문 안한 노드 찾기
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-def GCD(m, n, a):
-    a += 1
-    if(m < n):
-        m, n = n, m
-    
-    if (m % n == 0):
-        return n
-    else:
-        GCD(n, m % n, a)
-    print(f"hi from {a}")
+# 노드는 1번부터 사용하기 위해 첫번째 []
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
 
-m, n = map(int, input().split())
-
-print(GCD(m, n, 0))
+visited = [False] * 9
+dfs(graph, 1, visited)
